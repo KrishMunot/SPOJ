a059d394d532c8eb10f36966a68e3b79458d1d41
Krish Munot <krishmunot@gmail.com>
2017-01-07 00:00:01 +0530

    Update FCTRL.cpp

--- Solutions/FCTRL.cpp
+++ Solutions/FCTRL.cpp
@@ -5,16 +5,13 @@ int main()
 {
   int t,n,count;
   cin>>t;
-  while(t--)
- {
+  while(t--){
   count=0;
   cin>>n;
-  while(n>=5)
- {
+  while(n>=5){
   count += n/5;
   n /= 5;
- }
+  }
   cout<<count<<endl;
  }
-  return 0;
 }
