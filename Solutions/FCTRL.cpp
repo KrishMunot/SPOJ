b1528e02dc3f52f526cb8c9a5fec520b36832ef1
Krish Munot <krishmunot@gmail.com>
2017-01-06 23:59:25 +0530

    Create FCTRL.cpp

---
+++ Solutions/FCTRL.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  int t,n,count;
+  cin>>t;
+  while(t--)
+ {
+  count=0;
+  cin>>n;
+  while(n>=5)
+ {
+  count += n/5;
+  n /= 5;
+ }
+  cout<<count<<endl;
+ }
+  return 0;
+}
